cd9015d3aee692fbe38bc0943c3045cb57d49f34
John Freeman <jfreeman08@gmail.com>
2017-01-30 12:44:19 -0500

    Add ListThunk

---
+++ src/main/java/com/jfreeman/lazy/ListThunk.java
@@ -0,0 +1,86 @@
+package com.jfreeman.lazy;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * A lazy value that depends on a list of values of the same type.
+ *
+ * @param <T> the type of the value
+ * @param <E> the element type of the list
+ */
+public class ListThunk<T, E>
+    implements Lazy<T>
+{
+    private List<Lazy<E>> _deps;
+    private Function<List<E>, T> _func;
+    private T _value = null;
+
+    private ListThunk(
+        List<Lazy<E>> dependencies, Function<List<E>, T> function)
+    {
+        this._deps = dependencies;
+        this._func = function;
+    }
+
+    public static <T, E> ListThunk<T, E> of(
+        List<Lazy<E>> dependencies, Function<List<E>, T> function)
+    {
+        return new ListThunk<>(dependencies, function);
+    }
+
+    /**
+     * Essentially {@code sequenceA}: turn a {@code List} of {@code Lazy}s
+     * into a {@code Lazy} of a {@code List}.
+     *
+     * @param dependencies a list of lazy values
+     * @param <E> the type of the values
+     * @return a lazy list of values
+     */
+    public static <E> ListThunk<List<E>, E> of(
+        List<Lazy<E>> dependencies)
+    {
+        return ListThunk.of(dependencies, x -> x);
+    }
+
+    @Override
+    public boolean isForced() {
+        return _func == null;
+    }
+
+    @Override
+    public Iterable<? extends Lazy<?>> getDependencies()
+        throws IllegalStateException
+    {
+        if (isForced()) {
+            throw new IllegalStateException("already forced");
+        }
+        return _deps;
+    }
+
+    @Override
+    public T force()
+        throws IllegalStateException
+    {
+        if (isForced()) {
+            throw new IllegalStateException("already forced");
+        }
+        final List<E> args =
+            _deps.stream().map(Lazy::getValue).collect(Collectors.toList());
+        _value = _func.apply(args);
+        _func = null;
+        _deps = null;
+        return _value;
+    }
+
+    @Override
+    public T getValue()
+        throws IllegalStateException
+    {
+        if (!isForced()) {
+            throw new IllegalStateException("value not ready; have you forced?");
+        }
+        return _value;
+    }
+}
---
+++ src/test/java/com/jfreeman/lazy/ListThunkTest.java
@@ -0,0 +1,61 @@
+package com.jfreeman.lazy;
+
+import com.google.common.collect.ImmutableList;
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ListThunkTest
+{
+    private <T> void assertListEquals(List<T> expected, List<Lazy<T>> lazies) {
+        final Lazy<List<T>> lazyList = ListThunk.of(lazies);
+        final List<T> actual = LazyHelp.force(lazyList);
+        assertThat(actual, CoreMatchers.is(expected));
+    }
+
+    @Test
+    public void testEmptyList() {
+        final List<Lazy<Integer>> lazies = Collections.emptyList();
+        assertListEquals(Collections.emptyList(), lazies);
+    }
+
+    @Test
+    public void testSingletoneList() {
+        final List<Lazy<Integer>> lazies = ImmutableList.of(LazyHelp.delay(1));
+        assertListEquals(ImmutableList.of(1), lazies);
+    }
+
+    @Test
+    public void testList() {
+        final List<Lazy<Integer>> lazies = ImmutableList.of(
+            LazyHelp.delay(1),
+            LazyHelp.delay(2),
+            LazyHelp.delay(3));
+        assertListEquals(ImmutableList.of(1, 2, 3), lazies);
+    }
+
+    @Test
+    public void testInterdependentList() {
+        final Lazy<Integer> one = LazyHelp.delay(1);
+        final Lazy<Integer> two = LazyHelp.delay(one, x -> x + 1);
+        final Lazy<Integer> three = LazyHelp.delay(two, x -> x + 1);
+        final List<Lazy<Integer>> lazies = ImmutableList.of(one, two, three);
+        assertListEquals(ImmutableList.of(1, 2, 3), lazies);
+    }
+
+    @Test
+    public void testInterdependentSum() {
+        final Lazy<Integer> one = LazyHelp.delay(1);
+        final Lazy<Integer> two = LazyHelp.delay(one, x -> x + 1);
+        final Lazy<Integer> three = LazyHelp.delay(two, x -> x + 1);
+        final List<Lazy<Integer>> lazies = ImmutableList.of(one, two, three);
+        final Lazy<Integer> sum = ListThunk.of(lazies,
+            l -> l.stream().mapToInt(Integer::intValue).sum());
+        assertEquals(6, LazyHelp.force(sum).intValue());
+    }
+
+}
